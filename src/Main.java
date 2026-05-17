ca12297b816597708f822d7bdb9fa75079a4c471
mahaksingh080804 <mahak.singh080804>
2026-05-17 18:42:00 +0530

    backend setup updated

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,7 @@
 public class Main {
     public static void main(String[] args){
-        System.out.println("Welcome to my Mess Attendance System");
+        System.out.println("Backend setup is working");
     }
 
 }
+
